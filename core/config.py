310693ca751b5a46efe02c6b1c1df1f977ca3c1d
Pat Hearps <pat.hearps@gmail.com>
2025-02-25 23:22:54 +1100

    FEAT: allow for fetching kafka add from env var in cloud

--- core/config.py
+++ core/config.py
@@ -11,7 +11,7 @@ ENVIRONMENT = config("ENVIRONMENT", cast=str, default="local")
 if ENVIRONMENT == "local":
     KAFKA_ADDR = "localhost:9092"
 else:
-    KAFKA_ADDR = "???"
+    KAFKA_ADDR = config("KAFKA_ADDR", cast=str)
 
 URL_TRAFFIC = "https://data-exchange-api.vicroads.vic.gov.au/opendata/variable/freewaytraveltime/v1/traffic"
 
