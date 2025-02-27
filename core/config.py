62ae9f126d0b55d425da094aa3a27047bfd9a038
Pat Hearps <pat.hearps@gmail.com>
2025-02-27 12:34:45 +1100

    FIX: don't need kafka_add

--- core/config.py
+++ core/config.py
@@ -8,11 +8,6 @@ GCS_BUCKET = config("GCS_BUCKET", cast=str)
 
 ENVIRONMENT = config("ENVIRONMENT", cast=str, default="local")
 
-if ENVIRONMENT == "local":
-    KAFKA_ADDR = "localhost:9092"
-else:
-    KAFKA_ADDR = config("KAFKA_ADDR", cast=str)
-
 URL_TRAFFIC = "https://data-exchange-api.vicroads.vic.gov.au/opendata/variable/freewaytraveltime/v1/traffic"
 
 FWY_FILTER = "Eastern Fwy"
