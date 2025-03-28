b4cb24d208c377ce28b22bcf19fafac4b6a5f484
Pat Hearps <pat.hearps@gmail.com>
2025-03-28 11:37:26 +1100

    REFACT: put this as another cloud util

--- app/cloud.py
+++ app/cloud.py
@@ -1,3 +1,7 @@
+import io
+
+import polars as pl
+from google.cloud import bigquery
 import gcsfs
 
 from core.config import GCS_PROJECT
@@ -19,3 +23,27 @@ def write_df_pqt(df, destination: str):
         df.write_parquet(f)
     
     log.debug(f"Dataframe written to {destination}")
+
+
+def write_to_bigquery(df: pl.DataFrame, tablename: str, project: str = GCS_PROJECT):
+    client = bigquery.Client()
+    # Write DataFrame to stream as parquet file; does not hit disk
+    with io.BytesIO() as stream:
+        df.write_parquet(stream)
+        log.info("df written to bytestream")
+        stream.seek(0)  # reset to start of bytestream
+        parquet_options = bigquery.ParquetOptions()
+        parquet_options.enable_list_inference = True
+        job = client.load_table_from_file(
+            stream,
+            destination=tablename,
+            project=project,
+            job_config=bigquery.LoadJobConfig(
+                source_format=bigquery.SourceFormat.PARQUET,
+                parquet_options=parquet_options,
+            ),
+        )
+        log.info("job created, calling, waiting")
+    job.result()  # Waits for the job to complete
+    log.info("load job finished")
+
