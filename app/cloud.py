aa7cb2707a8bc64242ca91dd1c769d51be368642
Pat Hearps <pat.hearps@gmail.com>
2025-03-28 13:41:03 +1100

    FEAT: nest in the func that needs gcsfs

--- app/cloud.py
+++ app/cloud.py
@@ -9,13 +9,14 @@ from core.log_config import get_logger
 
 log = get_logger(__name__)
 
-log.debug("Trying to acquire gc filesystem")
-fs = gcsfs.GCSFileSystem(token="google_default",
-                         project=GCS_PROJECT,
-                         access="read_write")
-log.info("GCSFS acquired")
 
-def write_df_pqt(df, destination: str):
+
+def write_df_pqt(df: pl.DataFrame, destination: str) -> None:
+    log.debug("Trying to acquire gc filesystem")
+    fs = gcsfs.GCSFileSystem(token="google_default",
+                            project=GCS_PROJECT,
+                            access="read_write")
+    log.info("GCSFS acquired")
     log.debug(f"Attempting to open fs destination: {destination}")
     # Write the DataFrame to a Parquet file directly in GCS
     with fs.open(destination, mode='wb') as f:
