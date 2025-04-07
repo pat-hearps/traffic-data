ac21f80b7f2ebde756a9f985624b303efa57023c
Pat Hearps <pat.hearps@gmail.com>
2025-04-07 21:35:28 +1000

    REFACT: make global str const

--- app/to_bq.py
+++ app/to_bq.py
@@ -7,14 +7,14 @@ import app.cloud as acl
 from core.config import BQ_RAW_ZONE, GCS_BUCKET, TZ_MELB
 from core.log_config import get_logger
 
+LBL_FILEPATH = "raw_file_path"
+
 log = get_logger(__name__)
 
 
 def raw_to_loaded(dt_glob: str | None = None, raw_dir: str = "raw1", read_dir: str = "read1"):
     now = datetime.now(tz=TZ_MELB)
     uid = str(uuid.uuid4())
-
-    lbl_filepath = "raw_file_path"
 
     if dt_glob:
         try:
@@ -27,17 +27,17 @@ def raw_to_loaded(dt_glob: str | None = None, raw_dir: str = "raw1", read_dir: s
     gs_path = f"gs://{GCS_BUCKET}/{raw_dir}/{dt_glob}/*"
     log.info(f"Reading from {gs_path}")
 
-    df = pl.scan_parquet(gs_path, include_file_paths=lbl_filepath).collect()
+    df = pl.scan_parquet(gs_path, include_file_paths=LBL_FILEPATH).collect()
     log.info(f"read df from google cloud storage: {df.shape}")
     if len(df) == 0:
         log.info("No data found")
         return None
 
     # to send for moving to 'read' at end, get list prior to deduplication
-    all_file_paths = list(df.get_column(lbl_filepath).unique())
+    all_file_paths = list(df.get_column(LBL_FILEPATH).unique())
 
     # exclude raw_file_path col
-    dupe_cols = list(set(df.columns) - {lbl_filepath})
+    dupe_cols = list(set(df.columns) - {LBL_FILEPATH})
 
     df = df.unique(keep="last", subset=dupe_cols)
     df = df.with_columns(pl.lit(uid).alias("batch_uid"))
@@ -50,7 +50,7 @@ def raw_to_loaded(dt_glob: str | None = None, raw_dir: str = "raw1", read_dir: s
             "batch_uid": uid,
             "n_rows": df.shape[0],
             "n_columns": df.shape[1],
-            "n_raw_files": df.n_unique(subset=[lbl_filepath]),
+            "n_raw_files": df.n_unique(subset=[LBL_FILEPATH]),
             "schema": schema,
         }
     )
